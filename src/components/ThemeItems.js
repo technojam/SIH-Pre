33cbfc395d7006ffadb5af171090b748d9783ae0
Saurav <saurav.21scse1011071@galgotiasuniversity.edu.in>
2023-08-30 15:11:02 +0530

    theme card & icon  updated

--- src/components/ThemeItems.js
+++ src/components/ThemeItems.js
@@ -1,65 +1,24 @@
-import React, { Component } from 'react'
-import {
-  FacebookShareButton,FacebookIcon,TwitterShareButton,TwitterIcon,TelegramShareButton,TelegramIcon,WhatsappShareButton,WhatsappIcon
-} from "react-share";
-
-
+import React, { Component } from 'react';
 export default class Items extends Component {
   render() {
-    let {title, description, url}= this.props;
+    let { title, description, url,icon} = this.props;
     return (
-      <>
-      {/* Card Items for the card */}
-        <div className="card " style={{width: "18rem"}}>
-         <div>Icon</div>  
-
-  <div className="card-body">
-    
-    <h5 className="card-title">{title}...</h5>
-    <p className="card-text">{description}...</p>
-    <a  href={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Explore</a>
-      <hr />
-      {/* facebook share button */}
-      <div className="socialShare">
-
-      <FacebookShareButton 
-        url={url}
-        quote={"hi saurav"}
-        hashtag="#sauravpianist"
-      >
-      <FacebookIcon size={28} logoFillColor="white" round={true}></FacebookIcon>
-      </FacebookShareButton>
-      
-
-      {/* instagram share button  */}
-      <TwitterShareButton 
-        url={url}
-        quote={"hi saurav"}
-        hashtag="#React"
-      >
-      <TwitterIcon size={28} logoFillColor="white" round={true}></TwitterIcon>
-      </TwitterShareButton>
-
-      {/* telegram share button  */}
-      <TelegramShareButton 
-        url={url}
-        quote={"hi saurav"}
-        hashtag="#React"
-      >
-      <TelegramIcon size={28} logoFillColor="white" round={true}></TelegramIcon>
-      </TelegramShareButton>
-
-      <WhatsappShareButton 
-        url={url}
-        quote={"hi saurav"}
-        hashtag="#React"
-      >
-      <WhatsappIcon size={28} logoFillColor="white" round={true}></WhatsappIcon>
-      </WhatsappShareButton>
+      <div>
+        {/* Card Items for the card*/}
+        <div className="card cardtheme" style={{ width: "17rem", height: "20rem" }}>
+          <div className="card-body d-flex flex-column align-items-center text-center cartcolor">
+            <div className='mb-4'>
+              {/* <GiAutoRepair sx={{ fontSize: "80px", margin: "-2px" }} /> */}
+              <div >{icon}</div>
+            </div>
+            <h5 className="card-title" style={{ fontWeight: "800", marginTop: "1rem" }}>{title}</h5>
+            <p className="card-text" style={{ fontSize: "16px" }}>{description}...</p>
+            <div className="mt-auto d-flex align-items-center">
+              <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Explore</a>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>
-</div>
-      </>
     )
   }
 }
