89cd7ad5cc3e1503b153e5267d89ddad1b579fd2
M Sami <beboolean@gmail.com>
2023-09-05 00:36:34 +0530

    cards update

--- src/components/StaticCard.jsx
+++ src/components/StaticCard.jsx
@@ -6,148 +6,34 @@ import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
 import GroupsRoundedIcon from "@mui/icons-material/GroupsRounded";
 import AccessibilityNewRoundedIcon from "@mui/icons-material/AccessibilityNewRounded";
 import { motion } from "framer-motion";
+import "./flip.css"
+import {BsClock} from 'react-icons/bs'
+import {FaBookReader} from 'react-icons/fa'
+import {FaFaceGrinHearts} from 'react-icons//fa6'
 export default function StaticCard() {
 	return (
-		<motion.div
-			style={{ background: "#070c1d", paddingTop: "5em" }}
-		>
-			<Container maxWidth="xl">
-				<Stack
-					direction="row"
-					spacing={5}
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Box
-						sx={{
-							display: "grid",
-							gridTemplateColumns: "repeat(4, 1fr)",
-							gridGap: "10px",
-							justifyContent: "center",
-							alignItems: "center",
-							"@media (max-width: 600px)": {
-								gridTemplateColumns: "repeat(1, 1fr)",
-							},
-						}}
-					>
-						<Box
-							sx={{
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								alignItems: "center",
-								my: 2,
-								mx: 4,
+		<>  <div id='themes' className='con' >
+      
+        <div className="card cardtheme" style={{ width: "17rem", height: "20rem" }}>
+          <div className="card-body d-flex flex-column align-items-center text-center cartcolor">
+           <BsClock className="logo"/>
+            <h1 className="card-title" style={{ fontWeight: "800" }}>24 HRS</h1>
+          </div>
+        </div>
 
-								height: "370px",
-								width: "300px",
-								background:
-									"linear-gradient(180deg, #63f5bd, #00ffff)",
-								borderRadius: "8px",
-								boxShadow: "0px 0px 10px #000000",
-							}}
-						>
-							<AccessTimeIcon
-								fontSize="large"
-								sx={{ fontSize: "65px" }}
-							/>
-							<Typography
-								variant="h4"
-								textAlign="center"
-								sx={{ color: "#000" }}
-							>
-								24 Hrs
-							</Typography>
-						</Box>
-						<Box
-							sx={{
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								alignItems: "center",
-								height: "370px",
-								width: "300px",
-								my: 2,
-								mx: 4,
-								background:
-									"linear-gradient(180deg, #63f5bd, #00ffff)",
-								borderRadius: "8px",
-								boxShadow: "0px 0px 10px #000000",
-							}}
-						>
-							<LocalLibraryIcon
-								fontSize="large"
-								sx={{ fontSize: "100px" }}
-								paddingBottom="40px"
-							/>
-							<Typography
-								variant="h4"
-								textAlign="center"
-								sx={{ color: "#000" }}
-							>
-								16 Themes
-							</Typography>
-						</Box>
+		<div className="card cardtheme" style={{ width: "17rem", height: "20rem" }}>
+          <div className="card-body d-flex flex-column align-items-center text-center cartcolor">
+           <FaBookReader className="logo"/>
+            <h1 className="card-title" style={{ fontWeight: "800" }}>16 THEMES</h1>
+          </div>
+        </div>
 
-						<Box
-							sx={{
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								alignItems: "center",
-								height: "370px",
-								width: "300px",
-								my: 2,
-								mx: 4,
-								background:
-									"linear-gradient(180deg, #63f5bd, #00ffff)",
-								borderRadius: "8px",
-								boxShadow: "0px 0px 10px #000000",
-							}}
-						>
-							<PaidIcon
-								fontSize="large"
-								sx={{ fontSize: "65px" }}
-							/>
-							<Typography
-								variant="h4"
-								textAlign="center"
-								sx={{ color: "#000" }}
-							>
-								Cash Prize 1 Lakh
-							</Typography>
-						</Box>
-						<Box
-							sx={{
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								alignItems: "center",
-								height: "370px",
-								width: "300px",
-								my: 2,
-								mx: 4,
-								background:
-									"linear-gradient(180deg, #63f5bd, #00ffff)",
-								borderRadius: "8px",
-								boxShadow: "0px 0px 10px #000000",
-							}}
-						>
-							<AccessibilityNewRoundedIcon
-								fontSize="large"
-								sx={{ fontSize: "65px" }}
-							/>
-							<Typography
-								variant="h4"
-								textAlign="center"
-								sx={{ color: "#000" }}
-							>
-								Exciting Swags (For every participant)
-							</Typography>
-						</Box>
-					</Box>
-				</Stack>
-			</Container>
-		</motion.div>
+		<div className="card cardtheme" style={{fontSize:"1px", width: "17rem", height: "20rem" }}>
+          <div className="card-body d-flex flex-column align-items-center text-center cartcolor">
+           <FaFaceGrinHearts className="logo1"/>
+            <h1 className="card-title" style={{fontWeight: "800",fontSize:"30px"}} >EXCITING SWAGS(for every participant)</h1>
+          </div>
+        </div>
+      </div></>
 	);
 }
